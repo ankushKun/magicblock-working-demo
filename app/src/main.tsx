beb5fdecda95652f9589a11ac5f47bc3388b0287
ankushKun <ankush4singh@gmail.com>
2025-12-02 16:18:45 +0530

    buffer fix

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,10 @@ import './index.css'
 import App from './app.tsx'
 import { ThemeProvider } from "@/components/theme-provider"
 import { WalletProvider } from "@/contexts/WalletProvider"
+import { Buffer } from 'buffer'
+
+// Polyfill Buffer for browser
+window.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
